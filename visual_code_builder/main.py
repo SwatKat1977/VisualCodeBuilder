a308cefbe8a3057004a324f64aefd31e42801d8b
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-07-19 19:39:19 +0100

    copyright

--- visual_code_builder/main.py
+++ visual_code_builder/main.py
@@ -1,3 +1,22 @@
+"""
+This source file is part of Visual Code Builder
+For the latest info, see https://github.com/SwatKat1977/VisualCodeBuilder
+
+Copyright 2025 Visual Code Builder Development Team
+
+This program is free software : you can redistribute it and /or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see < https://www.gnu.org/licenses/>.
+"""
 import sys
 from PySide6.QtWidgets import QApplication, QLabel
 
@@ -6,6 +25,5 @@ if __name__ == '__main__':
 
     label = QLabel("Test Label")
     label.show()
-
 
     sys.exit(app.exec())
