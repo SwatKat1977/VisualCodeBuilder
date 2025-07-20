10724dc4c5ed92d2246eb2954a71ee6925e66396
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-07-20 19:41:31 +0100

    Scene navigation (#3)
    Changes:
    * Added pylint resource file so QT imports don't throw false-positives
    * Added zooming functionality
    * Added middle mouse button click and drag
    * Added placeholders for future use of left and right mouse button click and release.
    * Corrected README filename and added contents

--- visual_code_builder/node_editor_window_graphics_view.py
+++ visual_code_builder/node_editor_window_graphics_view.py
@@ -32,6 +32,13 @@ class NodeEditorWindowGraphicsView(QtWidgets.QGraphicsView):
 
         self.setScene(self.graphics_scene)
 
+        self._zoom_clamped = True
+        self._zoom_factor = 1.25
+        self._current_zoom = 10
+        self._zoom_step = 1
+        self._zoom_min_range_clamp = 0
+        self._zoom_max_range_clamp = 10
+
     def initialise(self):
         # Set Anti-aliasing.
         self.setRenderHints(QtGui.QPainter.Antialiasing |
@@ -45,3 +52,99 @@ class NodeEditorWindowGraphicsView(QtWidgets.QGraphicsView):
         # Hide the scrollbars.
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+        # Force transform to anchor centre on mouse.
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.MiddleButton:
+            self._middle_mouse_button_press(event)
+
+        elif event.button() == QtCore.Qt.LeftButton:
+            self._left_mouse_button_press(event)
+
+        elif event.button() == QtCore.Qt.RightButton:
+            self._right_mouse_button_press(event)
+
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.MiddleButton:
+            self._middle_mouse_button_release(event)
+
+        elif event.button() == QtCore.Qt.LeftButton:
+            self._left_mouse_button_release(event)
+
+        elif event.button() == QtCore.Qt.RightButton:
+            self._right_mouse_button_release(event)
+
+        else:
+            super().mouseReleaseEvent(event)
+
+    def wheelEvent(self, event):
+        """
+        Overrides the QGraphicsView mouse wheel event method to implement zoom
+        functionality.
+        """
+        if event.angleDelta().y() > 0:
+            zoom_factor = self._zoom_factor
+            self._current_zoom += self._zoom_step
+
+        # Calculate zoom factor (out)
+        else:
+            zoom_factor = 1 / self._zoom_factor
+            self._current_zoom -= self._zoom_step
+
+        # Clamp the zoom to within the min/max range.
+        clamped = False
+        if self._current_zoom < self._zoom_min_range_clamp:
+            self._current_zoom = self._zoom_min_range_clamp
+            clamped = True
+
+        if self._current_zoom > self._zoom_max_range_clamp:
+            self._current_zoom = self._zoom_max_range_clamp
+            clamped = True
+
+        # Set scene scale
+        if not clamped or self._zoom_clamped is False:
+            self.scale(zoom_factor, zoom_factor)
+
+    def _middle_mouse_button_press(self, event):
+        release_event = QtGui.QMouseEvent(
+            QtCore.QEvent.MouseButtonRelease,
+            event.localPos(), event.screenPos(),
+            QtCore.Qt.LeftButton, QtCore.Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(release_event)
+
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+
+        fake_event = QtGui.QMouseEvent(event.type(),
+                                       event.localPos(),
+                                       event.screenPos(),
+                                       QtCore.Qt.LeftButton,
+                                       event.buttons() | QtCore.Qt.LeftButton,
+                                       event.modifiers())
+        super().mousePressEvent(fake_event)
+
+    def _left_mouse_button_press(self, event):
+        super().mousePressEvent(event)
+
+    def _right_mouse_button_press(self, event):
+        super().mousePressEvent(event)
+
+    def _middle_mouse_button_release(self, event):
+        fake_event = QtGui.QMouseEvent(event.type(),
+                                       event.localPos(),
+                                       event.screenPos(),
+                                       QtCore.Qt.LeftButton,
+                                       event.buttons() | QtCore.Qt.LeftButton,
+                                       event.modifiers())
+        super().mouseReleaseEvent(fake_event)
+        self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+
+    def _left_mouse_button_release(self, event):
+        super().mouseReleaseEvent(event)
+
+    def _right_mouse_button_release(self, event):
+        super().mouseReleaseEvent(event)
